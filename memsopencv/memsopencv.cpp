00ef105f961025f365a0cd4ea3ce5fd3254166a2
Levente Fodor <levente.fodor@hotmail.com>
2020-11-01 05:44:39 +0000

    first commit

---
+++ memsopencv/memsopencv.cpp
@@ -0,0 +1,138 @@
+//https://www.opencv-srf.com/2010/09/object-detection-using-color-seperation.html
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	//open the video file for reading
+	//VideoCapture cap("C:/Users/leven/opencv/HerdOfDeerRunningOnSnowyRoad.mp4");
+	VideoCapture cap(0);
+	//VideoCapture cap("http://192.168.0.102:4747/video?x.mjpeg");
+
+	// if not success, exit program
+	if (cap.isOpened() == false)
+	{
+		//cout << "Cannot open the video file" << endl;
+		cout << "Cannot open the video cam " << endl;
+		cin.get(); //wait for any key press
+		return -1;
+	}
+
+	//Define names of the window
+	String window_name_of_original_video = "Control";
+
+	// Create a window with above names
+	namedWindow(window_name_of_original_video, WINDOW_NORMAL);
+
+	int iLowH = 170;
+	int iHighH = 179;
+
+	int iLowS = 150;
+	int iHighS = 255;
+
+	int iLowV = 60;
+	int iHighV = 255;
+
+	//Create trackbars in "Control" window
+	createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+	createTrackbar("HighH", "Control", &iHighH, 179);
+
+	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	createTrackbar("HighS", "Control", &iHighS, 255);
+
+	createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+	createTrackbar("HighV", "Control", &iHighV, 255);
+
+	int iLastX = -1;
+	int iLastY = -1;
+
+	//Capture a temporary image from the camera
+	Mat imgTmp;
+	cap.read(imgTmp);
+
+	//Create a black image with the size as the camera output
+	Mat imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);
+
+	while (true)
+	{
+		Mat imgOriginal;
+		bool bSuccess = cap.read(imgOriginal); // read a new frame from video 
+
+		//Breaking the while loop at the end of the video
+		if (bSuccess == false)
+		{
+			//cout << "Found the end of the video" << endl;
+			cout << "Video camera is disconnected" << endl;
+			break;
+		}
+
+		Mat imgHSV;
+
+		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+		Mat imgThresholded;
+
+		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+
+		//morphological opening (removes small objects from the foreground
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		//morphological closing (removes small holes from the foreground)
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		//Calculate the moments of the thresholded image
+		Moments oMoments = moments(imgThresholded);
+
+		double dM01 = oMoments.m01;
+		double dM10 = oMoments.m10;
+		double dArea = oMoments.m00;
+
+		// if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+		if (dArea > 10000)
+		{
+			//calculate the position of the ball
+			int posX = dM10 / dArea;
+			int posY = dM01 / dArea;
+
+			if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
+			{
+				//Draw a red line from the previous point to the current point
+				line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0, 0, 255), 2);
+			}
+
+			iLastX = posX;
+			iLastY = posY;
+		}
+
+		imshow("Thresholded Image", imgThresholded); //show the thresholded image
+
+		imgOriginal = imgOriginal + imgLines;
+
+		//show the frames in the created windows
+		imshow("Original", imgOriginal); //show the original image
+
+		//wait for for 10 ms until any key is pressed.  
+		//If the 'Esc' key is pressed, break the while loop.
+		//If the any other key is pressed, continue the loop 
+		//If any key is not pressed withing 10 ms, continue the loop
+		int comm = waitKey(10);
+		if (comm == 27) {
+			cout << "Esc key is pressed by user. Stoppig the video" << endl;
+			break;
+		}
+	}
+
+	destroyAllWindows(); //Destroy all opened windows
+
+	return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
